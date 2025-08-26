139957837105ef9200078e4d55c2ddec5c9d853d
popoffapp <shumben88@gmail.com>
2025-08-26 12:52:49 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/static"; // ✅ Vercel static adapter
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
-  site: "https://popoffapp.io", // update if needed
+  site: "https://popoffapp.io",
   integrations: [mdx(), sitemap()],
   adapter: vercel(),
 });
+
